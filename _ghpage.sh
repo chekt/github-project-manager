42d89ae115bb495677fcb1c198c92a3fdf9c18d9
Trevor Seo <trevor.seo@chekt.com>
2017-08-06 23:37:04 +0900

    fixed ghpage script

--- _ghpage.sh
+++ _ghpage.sh
@@ -9,7 +9,7 @@ mv dist docs
 
 cp src/assets/404.html docs/404.html
 
-sed -i -e 's/\<\/head\>/\<base href\=\"https:\/\/chekt.github.io\/github-project-manager\/\"\>\<\/head\>/g'  docs/index.html
+sed -i -e 's/\<head\>/\<head\>\<base href\=\"https:\/\/chekt.github.io\/github-project-manager\/\"\>/g'  docs/index.html
 sed -i -e 's/src\=\//src\=/g'  docs/index.html
 sed -i -e 's/href\=\//href\=/g'  docs/index.html
 
